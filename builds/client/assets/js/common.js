319282001f3e2aa0bdd4708bab7217fecf0a378a
puweb-mtis <104874657+puweb-mtis@users.noreply.github.com>
2022-11-09 16:21:43 +0900

    js: 스크립트 추가 및 수정
    - 헤더 서브 네비게이션 메뉴 추가
    - 헤더 스크롤 시 애니메이션 추가
    - 채용관련 swiper 추가
    - 네비게이션 메뉴 버튼 수정

--- builds/client/assets/js/common.js
+++ builds/client/assets/js/common.js
@@ -94,18 +94,45 @@ function swiperEachProgressbar(swiper) {
 }
 
 const $header = document.querySelector('.header');
-const $menuButton = document.querySelector('.btn-menu');
+const $menuOpenButton = document.querySelector('.header .btn-menu');
+const $menuCloseButton = document.querySelector('.nav .btn-menu');
 const $nav = document.querySelector('nav');
+const $headerNavSelects = $header.querySelectorAll('.nav-select');
 
-$menuButton.addEventListener('click', e => {
-    $nav.classList.toggle('active');
-    $header.classList.toggle('nav-open');
-    
-    if (e.target.tagName === 'SPAN') {
-        e.target.parentElement.classList.toggle('active');
+$headerNavSelects.forEach(($select, index, array) => {
+    $select.addEventListener('click', e => {
+        $select.classList.toggle('active');
+        
+        for (let i = 0; i < array.length; i++) {
+            if (i !== index) {
+                array[i].classList.remove('active');
+            }
+        }
+    });
+});
+
+window.addEventListener('scroll', e => {
+    if (window.scrollY > 0) {
+        $header.classList.add('scrolled');
     } else {
-        e.target.classList.toggle('active');
+        $header.classList.remove('scrolled');
     }
+});
+
+$menuOpenButton.addEventListener('click', e => {
+    $header.classList.add('nav-open');
+    $nav.classList.add('active');
+    
+    // if (e.target.tagName === 'SPAN') {
+    //     e.target.parentElement.classList.toggle('active');
+    // } else {
+    //     e.target.classList.toggle('active');
+    // }
+});
+
+$menuCloseButton.addEventListener('click', e => {
+    $header.classList.remove('nav-open');
+    $nav.classList.remove('active');
 });
 
 const introLocationSlider = document.querySelector('.intro-location__title-slider');
@@ -176,13 +203,13 @@ function lettersAnime(target, effect = 'letter') {
     
     for (let i = 0; i < $target.length; i++) {
         const letterWrappers = $target[i].querySelectorAll('[data-letters-wrapper]');
-    
+        
         switch (effect) {
             case 'letter':
                 for (let i = 0; i < letterWrappers.length; i++) {
                     const lettersWrapper = letterWrappers[i];
                     lettersWrapper.innerHTML = lettersWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-                
+                    
                     anime.timeline({loop: false})
                         .add({
                             targets: '[data-letters-wrapper] .letter',
@@ -197,13 +224,13 @@ function lettersAnime(target, effect = 'letter') {
                 for (let i = 0; i < letterWrappers.length; i++) {
                     const lettersWrapper = letterWrappers[i];
                     const letterArray = lettersWrapper.textContent.split(/\s/g);
-                
+                    
                     lettersWrapper.innerHTML = '';
-                
+                    
                     for (let j = 0; j < letterArray.length; j++) {
                         lettersWrapper.innerHTML += `<span class="letter">${letterArray[j]}</span>&nbsp;`;
                     }
-                
+                    
                     anime.timeline({loop: false})
                         .add({
                             targets: '[data-letters-wrapper] .letter',
@@ -221,9 +248,11 @@ function lettersAnime(target, effect = 'letter') {
 }
 
 const $topButton = document.querySelector('.btn-top');
-$topButton.addEventListener('click', e => {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-});
+if ( $topButton ) {
+    $topButton.addEventListener('click', e => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+}
 
 window.addEventListener('scroll', e => {
     const endPoint = document.body.scrollHeight - 305;
@@ -245,7 +274,7 @@ window.addEventListener('scroll', e => {
 });
 
 let prdViewSwiper;
-if ( document.querySelector('.prd-swiper .swiper') ) {
+if (document.querySelector('.prd-swiper .swiper')) {
     prdViewSwiper = new Swiper('.prd-swiper .swiper', {
         pagination: {
             el: '.prd-swiper .swiper-pagination--custom',
@@ -280,8 +309,117 @@ if ($droidSwipers.length > 0) {
                 nextEl: $swiperBox.querySelector('.swiper-button--next')
             }
         });
-    
+        
         swiperPaginationNumber(droidSwipers[i], '.droid-swiper');
+    }
+}
+
+const recruitIndex = document.querySelector('.recruit-index');
+
+if (recruitIndex) {
+    const recruitIndexSwiper1 = new Swiper('.index-swiper--1 .swiper', {
+        autoplay: {
+            delay: 4000
+        },
+        speed: 500,
+        navigation: {
+            prevEl: '.index-swiper--1 .swiper-button--prev',
+            nextEl: '.index-swiper--1 .swiper-button--next'
+        }
+    });
+    
+    swiperEachProgressbar(recruitIndexSwiper1);
+    
+    const $swiper1Players = document.querySelectorAll('.index-swiper--1 .player');
+    const $swiper1PlayButtons = document.querySelectorAll('.index-swiper--1 .swiper-button--play');
+    const $swiper1PauseButtons = document.querySelectorAll('.index-swiper--1 .swiper-button--pause');
+    
+    for (let i = 0; i < $swiper1PlayButtons.length; i++) {
+        $swiper1PlayButtons[i].addEventListener('click', e => {
+            recruitIndexSwiper1.autoplay.start();
+            
+            for (let j = 0; j < $swiper1Players.length; j++) {
+                $swiper1Players[i].classList.add('active');
+            }
+        });
+        
+        $swiper1PauseButtons[i].addEventListener('click', e => {
+            recruitIndexSwiper1.autoplay.stop();
+            
+            for (let j = 0; j < $swiper1Players.length; j++) {
+                $swiper1Players[i].classList.remove('active');
+            }
+        });
+    }
+    
+    const recruitIndexSwiper2 = new Swiper('.index-swiper--2 .swiper', {
+        autoplay: {
+            delay: 4500
+        },
+        speed: 500,
+        navigation: {
+            prevEl: '.index-swiper--2 .swiper-button--prev',
+            nextEl: '.index-swiper--2 .swiper-button--next'
+        }
+    });
+    
+    swiperEachProgressbar(recruitIndexSwiper2);
+    
+    const $swiper2Players = document.querySelectorAll('.index-swiper--2 .player');
+    const $swiper2PlayButtons = document.querySelectorAll('.index-swiper--2 .swiper-button--play');
+    const $swiper2PauseButtons = document.querySelectorAll('.index-swiper--2 .swiper-button--pause');
+    
+    for (let i = 0; i < $swiper2PlayButtons.length; i++) {
+        $swiper2PlayButtons[i].addEventListener('click', e => {
+            recruitIndexSwiper1.autoplay.start();
+            
+            for (let j = 0; j < $swiper2Players.length; j++) {
+                $swiper2Players[i].classList.add('active');
+            }
+        });
+        
+        $swiper2PauseButtons[i].addEventListener('click', e => {
+            recruitIndexSwiper1.autoplay.stop();
+            
+            for (let j = 0; j < $swiper2Players.length; j++) {
+                $swiper2Players[i].classList.remove('active');
+            }
+        });
+    }
+    
+    const recruitIndexSwiper3 = new Swiper('.index-swiper--3 .swiper', {
+        autoplay: {
+            delay: 5000
+        },
+        speed: 500,
+        navigation: {
+            prevEl: '.index-swiper--3 .swiper-button--prev',
+            nextEl: '.index-swiper--3 .swiper-button--next'
+        }
+    });
+    
+    swiperEachProgressbar(recruitIndexSwiper3);
+    
+    const $swiper3Players = document.querySelectorAll('.index-swiper--3 .player');
+    const $swiper3PlayButtons = document.querySelectorAll('.index-swiper--3 .swiper-button--play');
+    const $swiper3PauseButtons = document.querySelectorAll('.index-swiper--3 .swiper-button--pause');
+    
+    for (let i = 0; i < $swiper3PlayButtons.length; i++) {
+        $swiper3PlayButtons[i].addEventListener('click', e => {
+            recruitIndexSwiper1.autoplay.start();
+            
+            for (let j = 0; j < $swiper3Players.length; j++) {
+                $swiper3Players[i].classList.add('active');
+            }
+        });
+        
+        $swiper3PauseButtons[i].addEventListener('click', e => {
+            recruitIndexSwiper1.autoplay.stop();
+            
+            for (let j = 0; j < $swiper3Players.length; j++) {
+                $swiper3Players[i].classList.remove('active');
+            }
+        });
     }
 }
 
@@ -292,4 +430,22 @@ window.onload = e => {
     lettersAnime('.page-title--normal', 'word');
     lettersAnime('.page-title--line', 'word');
     lettersAnime('.page-title--banner');
+    
+    if (!document.querySelector('html').classList.contains('fp-enabled')) {
+        const currentPathname = window.location.pathname;
+        const currentDepth1 = window.location.pathname.split('/')[1];
+        const currentDepth2 = window.location.pathname.split('/')[2];
+        const currentNavDepth1Link = document.querySelector(`.nav .nav-list.depth-1 > .nav-list__item > a[href*="${currentDepth1}"]`);
+        const currentNavDepth2 = currentNavDepth1Link.nextElementSibling;
+        const currentNavDepth2Link = currentNavDepth2.querySelector(`a[href*="${currentDepth1}/${currentDepth2.substring(0, 5)}"]`);
+        const headerNavDepth1 = document.querySelector('.header .nav-box .nav-list.depth-1');
+        const headerNavDepth1Select = headerNavDepth1.closest('.nav-item').querySelector('.nav-select');
+        const headerNavDepth1Link = headerNavDepth1.querySelectorAll('.nav-list__item a');
+        const headerNavDepth2 = document.querySelector('.header .nav-box .nav-list.depth-2');
+        const headerNavDepth2Select = headerNavDepth2.closest('.nav-item').querySelector('.nav-select');
+        
+        headerNavDepth2.innerHTML = currentNavDepth2.innerHTML;
+        headerNavDepth1Select.innerHTML = currentNavDepth1Link.innerHTML;
+        headerNavDepth2Select.innerHTML = currentNavDepth2Link.innerHTML;
+    }
 }
